506e0b21e6bb12c21a6d18e7ae639834a851f69d
jrdeveloper124 <jrdeveloper124@gmail.com>
2023-04-21 14:28:57 +0530

    Uploading project files

---
+++ main.py
@@ -0,0 +1,95 @@
+from uuid import uuid4
+
+import boto3
+from botocore.exceptions import ClientError
+import magic
+import uvicorn
+from fastapi import FastAPI, HTTPException, Response, UploadFile, status
+from loguru import logger
+
+# session = boto3.Session(
+#     aws_access_key_id='ACCESS_KEY',
+#     aws_secret_access_key='SECRET_KEY',
+# )
+
+KB = 1024
+MB = 1024 * KB
+
+SUPPORTED_FILE_TYPES = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'application/pdf': 'pdf'
+}
+
+AWS_BUCKET = 'my-bucket-124'
+
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(AWS_BUCKET)
+
+
+async def s3_upload(contents: bytes, key: str):
+    logger.info(f'Uploading {key} to s3')
+    bucket.put_object(Key=key, Body=contents)
+
+
+async def s3_download(key: str):
+    try:
+        return s3.Object(bucket_name=AWS_BUCKET, key=key).get()['Body'].read()
+    except ClientError as err:
+        logger.error(str(err))
+
+app = FastAPI()
+
+
+@app.get('/')
+async def home():
+    return {'message': 'Hello from file-upload 😄👋'}
+
+
+@app.post('/upload')
+async def upload(file: UploadFile | None = None):
+    if not file:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='No file found!!'
+        )
+
+    contents = await file.read()
+    size = len(contents)
+
+    if not 0 < size <= 1 * MB:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Supported file size is 0 - 1 MB'
+        )
+
+    file_type = magic.from_buffer(buffer=contents, mime=True)
+    if file_type not in SUPPORTED_FILE_TYPES:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f'Unsupported file type: {file_type}. Supported types are {SUPPORTED_FILE_TYPES}'
+        )
+    file_name = f'{uuid4()}.{SUPPORTED_FILE_TYPES[file_type]}'
+    await s3_upload(contents=contents, key=file_name)
+    return {'file_name': file_name}
+
+
+@app.get('/download')
+async def download(file_name: str | None = None):
+    if not file_name:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='No file name provided'
+        )
+
+    contents = await s3_download(key=file_name)
+    return Response(
+        content=contents,
+        headers={
+            'Content-Disposition': f'attachment;filename={file_name}',
+            'Content-Type': 'application/octet-stream',
+        }
+    )
+
+if __name__ == '__main__':
+    uvicorn.run(app='main:app', reload=True)
